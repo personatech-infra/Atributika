1d9616ba83dd734d90e592e6d04c24a26ce076d7
Priyadarshan Meshram <priyadarshan@personatech.com>
2023-06-01 15:25:41 +0530

    spm update

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(name: "Atributika",
                       platforms: [.macOS(.v10_10),
-                                  .iOS(.v9),
+                                  .iOS(.v12),
                                   .tvOS(.v9),
                                   .watchOS(.v2)],
                       products: [.library(name: "Atributika",
